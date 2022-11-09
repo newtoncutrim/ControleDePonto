08c296a3f0029f321997c3e707c2e218a57be3ee
projetosnewton <newtoncutrim777@gmail.com>
2022-11-09 08:30:38 -0300

    ajuste

--- www/src/views/template/header.php
+++ www/src/views/template/header.php
@@ -31,8 +31,13 @@
 
         </div>
         <div class="dropdown">
+            <!-- aqui erro -->
             <div class="dropdown-button">
-                <span class="ml-2">Usuario Mock</span>
+                <img class="avatar"
+                    src="<?= "http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower(trim($_SESSION['user']->email)))?>" alt="user">
+                <span class="ml-2">
+                    <?= $_SESSION['user']->name?>
+                </span>
                 <i class="icofont-simple-down mx-2"></i>
                 <div class="dropdown-content">
                     <ul class="nav-list">
