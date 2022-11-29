fbac31b4df2c7e2b4e85939a210123e59e7bb9a7
newtoncutrim <newtoncutrim777@gmail.com>
2022-11-29 09:02:19 -0300

    horas trabalhadas

--- www/src/controllers/monthly_report.php
+++ www/src/controllers/monthly_report.php
@@ -13,13 +13,34 @@ $sumOfWorkedTime = 0;
 $lastDay = getLastDayOfMonth($currentDate)->format('d');
 
 for($day = 1; $day <= $lastDay; $day++) {
-    $date = $currentDate->format('Y-m') . '-' . $day;
-    echo $day . '</br>';
+    $date = $currentDate->format('Y-m') . '-' .sprintf('%02d', $day);
+    $registry = $registries[$date];
+
+    if(isPastWorkday($date)) $workDay++;
+
+    if($registry) {
+        $sumOfWorkedTime += $registry->worked_time;
+        array_push($report, $registry);
+    } else {
+        array_push($report, new WorkingHours([
+            'work_date' => $date,
+            'worked_time' => 0
+        ]));
+    }
 }
 
-// LoadTemplateView('monthly_report', [
-//     'registries' => $registries
-// ]);
+$expectedTime = $workDay * DAILY_TIME;
+$balance = getTimeStringFromSeconds(abs($sumOfWorkedTime - $expectedTime));
 
-//eu sou eu
+$sign = ($sumOfWorkedTime >= $expectedTime) ? "+" : '-';
+
+
+
+
+LoadTemplateView('monthly_report', [
+    'report' => $report,
+    'sumOfWorkedTime' => getTimeStringFromSeconds($sumOfWorkedTime),
+    'balance' => "{$sign}$balance"
+]);
+
 
--- www/src/views/monthly_report.php
+++ www/src/views/monthly_report.php
@@ -1,3 +1,43 @@
-<?php
+<main class="content">
+    <?php
 
-// print_r($registries);
+    renderTitle(
+        'Relatorio Mensal',
+        'Acompanhe seu saldo de horas',
+        'icofont-ui-calendar'
+    );
+    ?>
+
+    <div>
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <th>Dia</th>
+                <th>Entrada 1</th>
+                <th>Saida 1</th>
+                <th>Entrada 2</th>
+                <th>Saida 2</th>
+                <th>Saldo</th>
+            </thead>
+            <tbody>
+                <?php
+                foreach($report as $registry) { ?>
+                    <tr>
+                        <td><?= $registry->work_date ?></td>
+                        <td><?= $registry->time1 ?></td>
+                        <td><?= $registry->time2 ?></td>
+                        <td><?= $registry->time3 ?></td>
+                        <td><?= $registry->time4 ?></td>
+                        <td><?= 'Saldo' ?></td>
+                    </tr>
+                <?php } ?>
+                <tr class="bg-primary text-white">
+                    <td>Horas Trabalhadas</td>
+                    <td colspan="3"><?= $sumOfWorkedTime ?></td>
+                    <td>Saldo Mensal</td>
+                    <td><?= $balance ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</main>
